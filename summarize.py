1361164c8fdc33fb0240ca483441d0c3169ff98d
Edward Ng <66270109+edng5@users.noreply.github.com>
2023-01-30 13:42:06 -0500

    refactored to MVC framework

--- summarize.py
+++ summarize.py
@@ -2,123 +2,14 @@
 # Summarizes text and extracts the key information. Supports PDF, websites and audio file inputs.
 # 1/22/2023
 
-from newspaper import Article
-import spacy, speech_recognition
-from spacy.lang.en.stop_words import STOP_WORDS
-from string import punctuation
-from heapq import nlargest
 import tkinter as tk
 from tkinter import filedialog
-from PyPDF2 import PdfReader
-from tkPDFViewer import tkPDFViewer as pdf
+from utils import ShowPdf
 
-from transformers import PegasusForConditionalGeneration
-from transformers import PegasusTokenizer
-from transformers import pipeline
+from summary_controller import *
+from summary_model import *
 
-
-def text_rank_summarize(text: str, per: float) -> str:
-    '''
-    Uses the text rank algorithm to produce an extractive text summarization.
-    text: The text to summarize.
-    per: percentage of sentence.
-    :returns: string of summarized text.
-    '''
-    # load English pipeline
-    nlp = spacy.load('en_core_web_sm')
-    doc= nlp(text)
-    # tokens=[token.text for token in doc]
-
-    # normalize word frequencies and rank sentences to find importance
-    word_freq={}
-    for word in doc:
-        # filter out STOP_WORDS
-        if word.text.lower() not in list(STOP_WORDS) and word.text.lower() not in punctuation:
-            if word.text not in word_freq.keys():
-                word_freq[word.text] = 1
-            else:
-                word_freq[word.text] += 1
-    max_freq=max(word_freq.values())
-    for word in word_freq.keys():
-        word_freq[word]=word_freq[word]/max_freq
-    sentence_tokens= [sentence for sentence in doc.sents]
-    sentence_scores = {}
-    for sentence in sentence_tokens:
-        for word in sentence:
-            if word.text.lower() in word_freq.keys():
-                if sentence not in sentence_scores.keys():                            
-                    sentence_scores[sentence]=word_freq[word.text.lower()]
-                else:
-                    sentence_scores[sentence]+=word_freq[word.text.lower()]
-    select_length=int(len(sentence_tokens)*per)
-    summary=nlargest(select_length, sentence_scores,key=sentence_scores.get)
-    final_summary=[word.text for word in summary]
-    summary='\n'.join(final_summary)
-    return summary 
-
-
-def pegasus_summarize(text: str) -> str:
-    '''
-    Uses Pegasus pretrained model to produce abstractive text summarization.
-    :text - the text to summarize
-    :returns: string of summarized text.
-    '''
-    model = "google/pegasus-xsum"
-    pegasus_tokenizer = PegasusTokenizer.from_pretrained(model)
-
-    # Define PEGASUS model
-    pegasus_model = PegasusForConditionalGeneration.from_pretrained(model)
-
-    # Create tokens
-    tokens = pegasus_tokenizer(text, truncation=True, padding="longest", return_tensors="pt")
-
-    # Summarize text
-    encoded_summary = pegasus_model.generate(**tokens)
-
-    # Decode summarized text
-    decoded_summary = pegasus_tokenizer.decode(
-        encoded_summary[0],
-        skip_special_tokens=True
-    )
-
-    # Define summarization pipeline 
-    summarizer = pipeline(
-        "summarization", 
-        model=model, 
-        tokenizer=pegasus_tokenizer, 
-        framework="pt"
-    )
-
-    summary = summarizer(text, min_length=30, max_length=150)
-
-    return summary[0]["summary_text"]
-
-
-def extract_text(path: str) -> str:
-    '''
-    Takes in a path or url to a source and maps document format to extract text properly.
-    :path - a url or path to a source to extract text from.
-    :return: string of text to process on.
-    '''
-    # TODO: add pdf, video and audio mapping
-    if path[:3] in 'http':
-        article = Article(path)
-        article.download()
-        article.parse()
-        return article.text
-    elif path[-3:] in 'txt':
-        with open(path, 'r') as f:
-            text = f.read()
-        return text
-    elif path[-3:] in 'pdf':
-        reader = PdfReader(path)
-        page = reader.pages[0]
-        text = page.extract_text()
-        # text = ''
-        # for page in reader.pages:
-            # text = text + page.extract_text()
-        return text
-
+PDF_LOCATION = ""
 
 def browseFiles() -> None:
     '''
@@ -137,13 +28,16 @@ def browseFiles() -> None:
     path = filename
     text = extract_text(path)
     text = (text_rank_summarize(text,  0.20))
-    # can only pass a max length in pegasus?
+    # can only pass a max length 512 in pegasus?
     # text = pegasus_summarize(text)
     summary.configure(text=text)
     # v1.pdf_view(page_frame,
     #              pdf_location = filename, 
     #              width = 75, height = 100)
+    PDF_LOCATION = filename
 
+
+#==============MAIN=================#
 
 if __name__ == "__main__":
     # url = 'https://www.sciencedirect.com/science/article/pii/S2590005622000595'
@@ -183,11 +77,11 @@ if __name__ == "__main__":
     summary = tk.Label(work_frame, text='')
     summary.pack(side="bottom")
 
-    v1 = pdf.ShowPdf()
+    v1 = ShowPdf()
 
     filename = r"E:\Downloads\1-s2.0-S2590005622000595-main.pdf"
     pdf_viewer = v1.pdf_view(page_frame,
-                 pdf_location = filename, 
+                 pdf_location = PDF_LOCATION, 
                  width = 75, height = 100)
 
     #destroy and recreate to refresh the pdf viewer
@@ -198,5 +92,7 @@ if __name__ == "__main__":
     #              width = 75, height = 100)
 
     pdf_viewer.pack(side="left")
-
+    
+    # pdf_viewer.update_idletasks()
+    # root.after(0, v1.pdf_view(page_frame, pdf_location = PDF_LOCATION, width = 75, height = 100))
     root.mainloop()
